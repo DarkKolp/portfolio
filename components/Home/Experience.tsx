de7de9a98f326b1f26d5ee535b271515eef898f8
Nuno Silva <79986060+DarkKolp@users.noreply.github.com>
2023-03-16 18:53:32 +0000

    Update Experience.tsx

--- components/Home/Experience.tsx
+++ components/Home/Experience.tsx
@@ -21,6 +21,8 @@ const Experience = () => {
           <div className='absolute w-full h-[480px] left-0 top-0'>
             <div className='w-full h-full relative bg-black bg-opacity-70 hidden
             group-hover:inline-block transition-opacity duration-500'>
+              <h1 className='text-2xl text-center font-bold px-6 py-2 w-full 
+              absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 top-20'>College lab research</h1>
               <button 
               className='text-xl text-center font-bold bg-designColor px-6 py-2 w-60 
               absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 bottom-10'
