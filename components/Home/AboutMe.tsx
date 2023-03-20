7e31e939f494b65eadee1819d27edf2de5a2adbc
DarkKolp <nunoandresilva199@gmail.com>
2023-03-20 18:28:06 +0000

    Version 0.9 release

--- components/Home/AboutMe.tsx
+++ components/Home/AboutMe.tsx
@@ -15,28 +15,56 @@ const AboutMe = () => {
             md:text-2xl md:leading-relaxed
             lg:text-2xl
             2xl:text-3xl'>
-                Hi, I'm {" "}
+                Hi! My name is {" "}
                 <span className='font-bold tracking-wider text-white'>
                     Nuno Silva
-                </span>{" "} {"–"} {" "}
-                a self-taught{" "}
+                </span> {" "}
+                and I am self-driven{" "}
                 <span className='font-bold tracking-wider text-white'>
-                Blockchain, Web3 Developer and Researcher
-                </span> with a strong scientific background. Over the past three years, 
-                I've devoted thousands of hours to studying and researching Blockchain technologies,
-                 particularly their infrastructure, DeFi, Multichain, NFTs, and Metaverse. With my habilities to 
-                 acquire new knowledge and critical thinking skills, I bring a unique 
-                 perspective to Blockchain development and I am keen to tackle complex challenges in this exciting field.
+                Blockchain Developer and "Explorer"
+                </span> with a solid scientific background. Acknowledging the perks of the blockchain and its underlying 
+                technologies, such as smart-contracts and web3, to overcome, circumvent and even boost many limitations of 
+                contemporary technologies and propel our capabilities, I’ve decided to focus and pursue researching within 
+                this framework. For the past three years I’ve been researching and exploring its infrastructure – from Defi, 
+                Multichain NFTs and the Metaverse. Besides the obvious hardskills that I’ve acquired throughout this quest, 
+                such as …. I also acquired several softskills as I was searching to learn for new projects and concepts which 
+                provided me the opportunity to interact and learn along my peers from all over the world about this exciting field.
 
 
 
 
             </p>
-            <div className='w-[700px] grid grid-cols-1 md:grid-cols-2 gap-8 pt-[20px]'>
-                <AboutList title="Web Design" />
-                <AboutList title="Solidity" />
-                <AboutList title="Web3.js" />
+            <div className='pt-[20px] flex flex-col items-center justify-around 2xl:flex-row'>
+                <div className='flex flex-col'>
+                    <h1 className='text-2xl uppercase font-bold text-center 2xl:mr-[175px]'>Technical skills</h1>
+
+                    <div className='w-[700px] place-items-left grid grid-cols-2 gap-8 pt-[20px]'>
+                        <AboutList title="Solidity" />
+                        <AboutList title="Truffle/Hardhat" />
+                        <AboutList title="Web3.js/Ether.js" />
+                        <AboutList title="Javascript" />
+                        <AboutList title="React" />
+                        <AboutList title="Tailwind" />
+                        <AboutList title="Next.js" />
+                        <AboutList title="VBA" />
+                    </div>
+                </div>
+
+                <div className='pt-[50px] flex flex-col 2xl:pt-0 2xl:mb-[64px]'>
+                    <h1 className='text-2xl uppercase font-bold text-center 2xl:mr-[175px]'>Soft skills</h1>
+
+                    <div className='w-[700px] place-items-left grid grid-cols-2 gap-8 pt-[20px]'>
+                        <AboutList title="Adaptability" />
+                        <AboutList title="Communication" />
+                        <AboutList title="Curiosity" />
+                        <AboutList title="Willingness to learn" />
+                        <AboutList title="Ambition" />
+                        <AboutList title="Focus" />
+                    </div>
+                </div>
+
             </div>
+            
         </div>
     
     </section>
